02e333fe25f144983e56830717a545eef608c598
Stephan Wingerter <wingcommander2k7@runbox.com>
2022-11-28 23:31:58 +0100

    1st view

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
+  let clientName = "siggy";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("./assets/cropped-favicon.png")}
+        />
+      </View>
+      <View style={styles.caption}>
+        {clientName !== "" ? (
+          <Text>
+            Welcome back, <Text style={styles.boldText}>{clientName}</Text>
+          </Text>
+        ) : (
+          ""
+        )}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    flexDirection: "column",
+    alignItems: "center",
+    //justifyContent: 'center',
+    //padding: 5
+  },
+  logo: {
+    width: 128,
+    height: 128,
+  },
+  logoContainer: {
+    margin: 10,
+    flex: 1,
+    marginTop: 40,
+  },
+  caption: {
+    flex: 3,
+  },
+  boldText: {
+    fontWeight: "bold",
   },
 });
