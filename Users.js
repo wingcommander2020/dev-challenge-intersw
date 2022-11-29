af99f3543cabbb77e3ed89309f9f4511da277b46
Stephan Wingerter <wingcommander2k7@runbox.com>
2022-11-29 20:59:31 +0100

    User Files created

---
+++ Users.js
@@ -0,0 +1,16 @@
+export const users= [
+        {name: 'Tony Stark', login: 'tony.stark', password: 'avengers', accounts: [
+            {account:'1234 4565 78',balance:'7000111.58'},
+            {account:'4567 2588 99',balance:'1000300.11'},
+            {account:'2567 3558 59',balance:'6001300.11'},
+            {account:'7898 4565 89',balance:'1001300.11'},
+            {account:'3567 4568 86',balance:'1001320.11'},
+            {account:'8567 5538 79',balance:'5401320.11'},
+            {account:'7567 6588 29',balance:'5401020.11'},
+            {account:'9567 9578 29',balance:'5400020.11'},
+            {account:'6567 3598 39',balance:'5400020.11'},
+            {account:'2567 1548 49',balance:'5400000.11'}
+        ]
+        },
+        {name: 'Bruce Banner', login: 'bruce.banner', password: 'avengers', accounts: [{account:'2567 3228 59',balance:'300.11'}]}
+]
