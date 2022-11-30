d25a21d6c4b0afa533713e449e594fdddc309f13
Stephan Wingerter <wingcommander2k7@runbox.com>
2022-11-30 18:31:38 +0100

    Milestone: view2 almost done

--- components/MainView.js
+++ components/MainView.js
@@ -5,43 +5,191 @@ import {
   View,
   Image,
   TextInput,
-  Pressable,
+  TouchableOpacity,
 } from "react-native";
-import { users } from "../Users";
+import { TapGestureHandler, RotationGestureHandler } from 'react-native-gesture-handler';
+import { Feather } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function MainView() {
-    let clientName = "siggy";
-    let text = "";
-    let name = users[0].name;
-    return (
-      <View style={styles.container}>
-        <View style={styles.logoContainer}>
-          <Image
-            style={styles.logo}
-            source={require("../assets/cropped-favicon.png")}
-          />
-        </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Ionicons name="menu" size={36} color="#b02424" />
+        <Image
+          style={styles.logo}
+          source={require("../assets/cropped-favicon.png")}
+        />
+      </View>
+      <View style={styles.bodyContainer}>
+      </View>
+      
+      <View style={styles.carouselContainer}>
+        <Text style={{ fontSize: 18 }}>My Accounts</Text>
+      <View style={styles.accountCard}>
+      <Text style={{ fontSize: 18 }}>Account:</Text>
+      <Text style={{ fontSize: 18 }}>Balance:</Text>
+      </View>
+      </View>
 
-  
-          <StatusBar style="auto" />
+
+
+
+
+      <View style={styles.servicesContainer}>
+        <View style={styles.servicesRowContainer}>
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <TouchableOpacity title="service1" style={styles.serviceButton}>
+              <Image
+                style={styles.serviceIcon}
+                source={require("../assets/service_icon1.png")}
+              />
+            </TouchableOpacity>
+            <Text style={styles.pressableTitle}>Transfer</Text>
+          </View>
+
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <TouchableOpacity title="service2" style={styles.serviceButton}>
+              <Image
+                style={styles.serviceIcon}
+                source={require("../assets/service_icon2.png")}
+              />
+            </TouchableOpacity>
+            <Text style={styles.pressableTitle}>Pay Bills</Text>
+          </View>
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <TouchableOpacity title="service3" style={styles.serviceButton}>
+              <Image
+                style={styles.serviceIcon}
+                source={require("../assets/service_icon3.png")}
+              />
+            </TouchableOpacity>
+            <Text style={styles.pressableTitle}>Buy Airtime</Text>
+          </View>
         </View>
-    );
+        <View style={styles.servicesRowContainer}>
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <TouchableOpacity title="service4" style={styles.serviceButton}>
+              <Image
+                style={styles.serviceIcon}
+                source={require("../assets/service_icon4.png")}
+              />
+            </TouchableOpacity>
+            <Text style={styles.pressableTitle}>QR Payment</Text>
+          </View>
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <TouchableOpacity title="service5" style={styles.serviceButton}>
+              <Image
+                style={styles.serviceIcon}
+                source={require("../assets/service_icon5.png")}
+              />
+            </TouchableOpacity>
+            <Text style={styles.pressableTitle}>Loans</Text>
+          </View>
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <TouchableOpacity title="service6" style={styles.serviceButton}>
+              <Image
+                style={styles.serviceIcon}
+                source={require("../assets/service_icon6.png")}
+              />
+            </TouchableOpacity>
+            <Text style={styles.pressableTitle}>Virtual Card</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.footerContainer}>
+        <View style={styles.footerContent}>
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <Feather name="home" size={36} color="white" />
+            <Text style={{ color: "white" }}>Dashboard</Text>
+          </View>
+
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <Feather name="settings" size={36} color="white" />
+            <Text style={{ color: "white" }}>Settings</Text>
+          </View>
+        </View>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-//const styles = StyleSheet.create({});
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 2,
-      paddingTop: 64,
-      backgroundColor: "#fff",
-      flexDirection: "column",
-    },
-    logo: {
-      width: 36,
-      height: 36,
-    },
-    logoContainer: {
-      alignItems: "center",
-    }
-  });
+  container: {
+    flex: 1,
+    paddingTop: 48,
+    backgroundColor: "#fff",
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  logo: {
+    width: 36,
+    height: 36,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "base-line",
+    justifyContent: "space-between",
+    paddingBottom: 48,
+  },
+  carouselContainer:{
+    paddingRight: 0,
+    paddingTop:16,
+    height: '25%',
+    minWidth:256
+  },
+  accountCard:{
+    paddingTop:16,
+    paddingLeft:16,
+    height: 90,
+    width: "64%",
+    backgroundColor: "#f5f4f4",
+
+
+    borderRadius: 4,
+    shadowColor: '#7a7979',
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 1,
+    shadowRadius: 8,
+    elevation: 5,
+  },
+  servicesContainer: {},
+  serviceButton: {
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "#b7b7b7",
+    borderColor: "blue",
+    borderRadius: 100,
+    height: 64,
+    width: 64,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  serviceIcon: {
+    alignItems: "center",
+  },
+  footerContainer: {
+    position: "absolute",
+    flex: 0.1,
+    left: 0,
+    right: 0,
+    bottom: -10,
+    backgroundColor: "#b02424",
+    height: "16%",
+  },
+  footerContent: {
+    flexDirection: "row",
+    alignItems: "base-line",
+    justifyContent: "space-between",
+    paddingTop: 8,
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  servicesRowContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingTop: 32,
+  },
+
+});
