f8a6f815dbea5b09594f91a541890bf91f325168
Stephan Wingerter <wingerter@runbox.com>
2022-11-30 19:40:38 +0100

    back on win

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginView from "./components/LoginView";
 import MainView from "./components/MainView";
-import MenuModal
+import MenuModal from './components/MenuModal';
 import {
   Text,
   View,
@@ -17,7 +17,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   function ModalScreen({ navigation }) {
     return (
-      <View style={{alignItems: 'left', justifyContent: 'left' }}>
+      <View style={{ alignItems: 'left', justifyContent: 'left' }}>
         <Text style={{ fontSize: 30 }}>This is a modal!</Text>
         <Button onPress={() => navigation.goBack()} title="Dismiss" />
       </View>
@@ -27,13 +27,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Group>
-        <Stack.Screen name="Main" component={MainView} />
-      </Stack.Group>
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
-      <Stack.Screen name="menu" component={ModalScreen} />
+        <Stack.Group>
+          <Stack.Screen name="Main" component={MainView} />
+        </Stack.Group>
+        <Stack.Group screenOptions={{ presentation: 'modal' }}>
+          <Stack.Screen name="menu" component={ModalScreen} />
 
-      </Stack.Group>
+        </Stack.Group>
 
       </Stack.Navigator>
     </NavigationContainer>
